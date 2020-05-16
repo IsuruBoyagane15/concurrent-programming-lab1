3df11edaaadfcb74b1768bbfcdf7509949b997c3
dilanSachi <dilansachinthanos@gmail.com>
2020-05-16 19:07:02 +0530

    completed mutex lock and rw lock

--- LinkedList.c
+++ LinkedList.c
@@ -6,9 +6,10 @@
 
 unsigned bit;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_rwlock_t rw_lock = PTHREAD_RWLOCK_INITIALIZER;
 
 unsigned genUniqueRandNum(long *lfsr)
-{   
+{
     bit = ((*lfsr >> 0) ^ (*lfsr >> 2) ^ (*lfsr >> 3) ^ (*lfsr >> 5)) & 1;
     return *lfsr = (*lfsr >> 1) | (bit << 15);
 }
@@ -105,54 +106,14 @@ int Delete(int value, struct list_node_s **head_pp)
 
 void Traverse(struct list_node_s *node)
 {
-    struct list_node_s* traversingNode = node;
+    struct list_node_s *traversingNode = node;
     while (traversingNode != NULL)
     {
-        printf(" %d ", traversingNode -> data);
+        printf(" %d ", traversingNode->data);
         traversingNode = traversingNode->next;
     }
     printf("\n");
 }
-
-// #define ERR_NO_NUM -1
-// #define ERR_NO_MEM -2
-
-// int myRandom (int size) {
-//     int i, n;
-//     static int numNums = 0;
-//     static int *numArr = NULL;
-
-//     // Initialize with a specific size.
-
-//     if (size >= 0) {
-//         if (numArr != NULL)
-//             free (numArr);
-//         if ((numArr = malloc (sizeof(int) * size)) == NULL)
-//             return ERR_NO_MEM;
-//         for (i = 0; i  < size; i++)
-//             numArr[i] = i;
-//         numNums = size;
-//     }
-
-//     // Error if no numbers left in pool.
-
-//     if (numNums == 0)
-//         return ERR_NO_NUM;
-
-//     // Get random number from pool and remove it (rnd in this
-//     //   case returns a number between 0 and numNums-1 inclusive).
-
-//     n = rand() % numNums;
-//     i = numArr[n];
-//     numArr[n] = numArr[numNums-1];
-//     numNums--;
-//     if (numNums == 0) {
-//         free (numArr);
-//         numArr = 0;
-//     }
-
-//     return i;
-// }
 
 void serialProgram(long int *numOperations, struct list_node_s **head)
 {
@@ -185,19 +146,16 @@ void serialProgram(long int *numOperations, struct list_node_s **head)
 void *mutexProgram(void *ptr)
 {
     struct thread_args *args = (struct thread_args *)ptr;
-    long numOperations = args -> numOperations;
-    long threadId = args -> threadId;
+    long numOperations = args->numOperations;
+    long threadId = args->threadId;
     struct list_node_s **head = args->head;
 
     for (int i = 0; i < numOperations; ++i)
     {
-        pthread_mutex_lock(&lock);
         int randNum = genUniqueRandNum(&threadId);
-        pthread_mutex_unlock(&lock);
         int randomChoice = randNum % 3;
         if (randomChoice == 0)
         {
-            
             pthread_mutex_lock(&lock);
             printf("Member %d : ", randNum);
             int result = Member(randNum, *head);
@@ -206,7 +164,6 @@ void *mutexProgram(void *ptr)
         }
         else if (randomChoice == 1)
         {
-            
             pthread_mutex_lock(&lock);
             printf("Insert %d : ", randNum);
             int result = Insert(randNum, head);
@@ -215,62 +172,68 @@ void *mutexProgram(void *ptr)
         }
         else if (randomChoice == 2)
         {
-            
             pthread_mutex_lock(&lock);
             printf("Delete %d : ", randNum);
             int result = Delete(randNum, head);
             printf("%d\n", result);
             pthread_mutex_unlock(&lock);
         }
-        else
-        {
-            printf("none");
-        }
     }
-    pthread_mutex_destroy(&lock);
     return EXIT_SUCCESS;
 }
 
-void readWriteLockProgram(long int* numOperations, struct list_node_s* head) {
-    // pthread_rwlock_t rw_lock;
-    // if (pthread_rwlock_init(&rw_lock, NULL) != 0) {
-    //     printf("\n rw lock init has failed\n");
-    //     return 1;
-    // }
-    // long lfsr = time(0);
-    // for (int i = 0; i < *numOperations; ++i) {
-    //     int randomChoice = rand() % 3;
-    //     int randNum = genUniqueRandNum(&lfsr);
-    //     if (randomChoice == 0) {
-    //         printf("Member %d : ", randNum);
-    //         pthread_rwlock_rdlock(&rw_lock);
-    //         int result = Member(randNum, head);
-    //         printf("%d\n", result);
-    //         pthread_rwlock_unlock(&rw_lock);
-    //     } else if (randomChoice ==1) {
-    //         printf("Insert %d : ", randNum);
-    //         pthread_rwlock_wrlock(&rw_lock);
-    //         int result = Insert(randNum, &head);
-    //         printf("%d\n", result);
-    //         pthread_rwlock_wrlock(&rw_lock);
-    //     } else if (randomChoice == 2) {
-    //         printf("Delete %d : ", randNum);
-    //         pthread_rwlock_wrlock(&rw_lock);
-    //         int result = Delete(randNum, &head);
-    //         printf("%d\n", result);
-    //         pthread_rwlock_wrlock(&rw_lock);
-    //     }
-    // }
-    // pthread_rwlock_destroy(&rw_lock);
+void *readWriteLockProgram(void *ptr)
+{
+    printf("rw lock %p\n", &rw_lock);
+    struct thread_args *args = (struct thread_args *)ptr;
+    long numOperations = args->numOperations;
+    long threadId = args->threadId;
+    struct list_node_s **head = args->head;
+
+    for (int i = 0; i < numOperations; ++i)
+    {
+        int randNum = genUniqueRandNum(&threadId);
+        int randomChoice = randNum % 3;
+        if (randomChoice == 0)
+        {
+            pthread_rwlock_rdlock(&rw_lock);
+            printf("Member %d : ", randNum);
+            int result = Member(randNum, *head);
+            printf("%d\n", result);
+            pthread_rwlock_unlock(&rw_lock);
+        }
+        else if (randomChoice == 1)
+        {
+            pthread_rwlock_wrlock(&rw_lock);
+            printf("Insert %d : ", randNum);
+            int result = Insert(randNum, head);
+            printf("%d\n", result);
+            pthread_rwlock_unlock(&rw_lock);
+        }
+        else if (randomChoice == 2)
+        {
+            pthread_rwlock_wrlock(&rw_lock);
+            printf("Delete %d : ", randNum);
+            int result = Delete(randNum, head);
+            printf("%d\n", result);
+            pthread_rwlock_unlock(&rw_lock);
+        }
+    }
+    return EXIT_SUCCESS;
 }
 
 int main()
 {
-    struct list_node_s* head = NULL;
+    struct list_node_s *head = NULL;
     long numThreads;
     long numOperations;
 
     pthread_t *threadHandles;
+
+    // if (pthread_rwlock_init(&rw_lock, NULL) != 0) { 
+    //     printf("\n rwlock init has failed\n"); 
+    //     return 1; 
+    // }
 
     printf("Enter number of threads : ");
     scanf("%ld", &numThreads);
@@ -279,22 +242,23 @@ int main()
 
     threadHandles = malloc(numThreads * sizeof(pthread_t));
     long thread;
-    printf("pointer for thread id in main is %p\n",&thread);
+    printf("pointer for thread id in main is %p\n", &thread);
     for (thread = 0; thread < numThreads; ++thread)
     {
         struct thread_args *thread_args_struct = malloc(sizeof(struct thread_args));
-        thread_args_struct -> numOperations = numOperations;
-        thread_args_struct -> head = &head;
-        thread_args_struct -> threadId = thread + 1000;
-        pthread_create(&threadHandles[thread], NULL, mutexProgram, (void *)thread_args_struct);
+        thread_args_struct->numOperations = numOperations;
+        thread_args_struct->head = &head;
+        thread_args_struct->threadId = thread + 1000;
+        // pthread_create(&threadHandles[thread], NULL, mutexProgram, (void *)thread_args_struct);
+        pthread_create(&threadHandles[thread], NULL, readWriteLockProgram, (void *)thread_args_struct);
     }
-    for (thread = 0; thread < numThreads; ++thread)
+    int t;
+    for (t = 0; t < numThreads; ++t)
     {
-        pthread_join(threadHandles[thread], NULL);
+        pthread_join(threadHandles[t], NULL);
     }
     free(threadHandles);
     pthread_mutex_destroy(&lock);
-
     Traverse(head);
 
     return 0;
